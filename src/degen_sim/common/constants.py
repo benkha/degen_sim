d3829cd90931da687bb2fbd74427de952607d0b3
Benjamin Kha <ben.kha@outlook.com>
2025-10-07 21:57:37 -0400

    Setup repo (#1)
    * First commit
    
    * Add degen sim notebook
    
    * Add constants
    
    * Add notebook utils

---
+++ src/degen_sim/common/constants.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+
+import degen_sim
+
+ROOT_DIR = str(Path(degen_sim.__file__).parent.resolve())
+DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(ROOT_DIR)), "data")
