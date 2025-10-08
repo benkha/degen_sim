6f55ff0c3b3f52f1e726650e6b20b0fd6d710f7c
Benjamin Kha <ben.kha@outlook.com>
2025-10-07 22:40:28 -0400

    Update notebook

--- src/degen_sim/common/constants.py
+++ src/degen_sim/common/constants.py
@@ -5,3 +5,4 @@ import degen_sim
 
 ROOT_DIR = str(Path(degen_sim.__file__).parent.resolve())
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(ROOT_DIR)), "data")
+REPORTS_DIR = os.path.join(os.path.dirname(os.path.dirname(ROOT_DIR)), "reports")
